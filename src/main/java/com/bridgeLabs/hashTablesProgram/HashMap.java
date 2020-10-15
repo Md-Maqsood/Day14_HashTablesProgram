a81a96b328edf44bdf74fdded1f8b60a07528220
maqsood <maqsoodhamd37@gmail.com>
2020-10-15 17:14:59 +0530

    [Maqsood].add: initial commit

---
+++ src/main/java/com/bridgeLabs/hashTablesProgram/HashMap.java
@@ -0,0 +1,11 @@
+package com.bridgeLabs.hashTablesProgram;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HashMap {
+	private static final Logger logger=LogManager.getLogger(HashMap.class);
+	public static void main(String[] args) {
+		logger.debug("Welcome to hash tables program.");
+	}
+}
---
+++ src/test/java/com/bridgeLabs/hashTablesProgram/HashMapTest.java
@@ -0,0 +1,12 @@
+package com.bridgeLabs.hashTablesProgram;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HashMapTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
